11627f428372a78fe32aa82f0043e69d98c88bf6
abelopez89 <abelopez89@gmail.com>
2013-07-20 13:37:45 -0400

    Nuevo archivo inserts.sql
    SQL con los inserts iniciales para ejecutar el sistema

---
+++ basededatos/inserts.sql
@@ -0,0 +1,11 @@
+INSERT INTO usuario(
+            id, estado, nivel, nombre, password)
+    VALUES (0, 'A', 9, 'Sin nombre', '123');
+	
+INSERT INTO productos(
+            id, cantidad, descripcion, iva, observaciones, precio)
+    VALUES (1, 100, 'Producto 1', 100, 'Observacion 1', 10000);
+
+INSERT INTO productos(
+            id, cantidad, descripcion, iva, observaciones, precio)
+    VALUES (2, 50, 'Producto 2', 200, 'Observacion 2', 20000);	
